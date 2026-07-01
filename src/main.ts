76f20b03814b3ce625f9e86b74ec4a7e647c7291
Inna Saienko <inna.saienko.nl@gmail.com>
2026-07-01 10:27:13 +0200

    feat(exercises): update main.ts with proper initialization

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-import { initExercisesPage } from './pages/exercises-search';
+import { ExercisesModel } from './mvc/exercises.model';
+import { ExercisesView } from './mvc/exercises.view';
+import { ExercisesController } from './mvc/exercises.controller';
 
-  initExercisesPage();
+const exercisesSection = document.querySelector<HTMLElement>('[data-exercises-section]');
+if (!exercisesSection) throw new Error('Exercises section not found');
+
+const model = new ExercisesModel();
+const view = new ExercisesView(exercisesSection);
+view.renderExerciseCategories();
+const controller = new ExercisesController(model, view);
+
+controller.init();
 
