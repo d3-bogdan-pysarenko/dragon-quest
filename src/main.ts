4dbfd3957cd885b4f421b6608b3436e141c0249b
unknown <pankovdm@gmail.com>
2026-07-01 07:00:05 -0700

    add burger menu; add styles to page header

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { initRatingModal } from './components/rating-modal';
 import { showLoader, hideLoader } from './components/loader';
+import { initBurgerMenu } from './components/burger-menu';
 
 showLoader();
 hideLoader();
 
 initRatingModal();
+initBurgerMenu();
 
 export {};
